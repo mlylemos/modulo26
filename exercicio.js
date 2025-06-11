c67e951e57af7e63a74d2b32e821854b25b302ed
Emily <emilylemosn.u@gmail.com>
2025-06-11 19:36:55 -0300

    commit inicial

---
+++ exercicio.js
@@ -0,0 +1,8 @@
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log("Resultado da multiplicação:", multiplicar(7, 4));
+console.log("Mensagem de saudação:", saudacao("Mundo"));
---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log("Resultado da multiplicação:", multiplicar(7, 4));
+console.log("Mensagem de saudação:", saudacao("Mundo"));
